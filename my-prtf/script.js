ac419af7a69d18762bf57fa0dee6b9996a547a3c
nickshakh <shakhbazyann@gmail.com>
2023-06-05 23:41:47 +0400

    ,

---
+++ my-prtf/script.js
@@ -0,0 +1,11 @@
+function Nick () {
+    var x = document.getElementById("nav-div")
+
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+
+
